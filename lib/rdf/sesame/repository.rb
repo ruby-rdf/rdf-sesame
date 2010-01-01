5757bd9a9d8348182f2f11b470e719a954634fbc
Arto Bendiken <arto@bendiken.net>
2010-01-01 02:51:28 +0100

    Implemented RDF::Sesame::Repository#initialize.

--- lib/rdf/sesame/repository.rb
+++ lib/rdf/sesame/repository.rb
@@ -1,27 +1,81 @@
-module RDF module Sesame
+module RDF::Sesame
   ##
-  # A repository on a Sesame 2.0 HTTP server.
+  # A repository on a Sesame 2.0-compatible HTTP server.
   #
-  # @example Connecting to a repository
-  #   url = RDF::URI.new("http://localhost:8080/openrdf-sesame/repositories/")
-  #   db  = RDF::Sesame::Repository.new(:url => url)
+  # Instances of this class represent RDF repositories on Sesame-compatible
+  # servers.
   #
+  # @example Opening a Sesame repository (1)
+  #   url    = "http://localhost:8080/openrdf-sesame/repositories/SYSTEM"
+  #   db     = RDF::Sesame::Repository.new(url)
+  #
+  # @example Opening a Sesame repository (2)
+  #   server = RDF::Sesame::Server.new("http://localhost:8080/openrdf-sesame")
+  #   db     = RDF::Sesame::Repository.new(:server => server, :id => :SYSTEM)
+  #
+  # @example Opening a Sesame repository (3)
+  #   server = RDF::Sesame::Server.new("http://localhost:8080/openrdf-sesame")
+  #   db     = server.repository(:SYSTEM)
+  #
+  # @see RDF::Sesame
   # @see http://www.openrdf.org/doc/sesame2/system/ch08.html
   class Repository < RDF::Repository
+    # @return [RDF::URI]
+    attr_reader :uri
     alias_method :url, :uri
 
-    # @return [Connection]
-    attr_reader :connection
+    # @return [String]
+    attr_reader :id
+
+    # @return [String]
+    attr_reader :title
+
+    # @return [Server]
+    attr_reader :server
 
     ##
-    # @param  [Hash{Symbol => Object}] options
-    # @option options [RDF::URI] :url (nil)
-    # @yield  [repository]
-    # @yieldparam [Repository]
-    def initialize(options = {}, &block)
-      @title   = options.delete(:title) if options.has_key?(:title)
-      @uri     = options.delete(:url) || options.delete(:uri)
-      @options = options
+    # Initializes this `Repository` instance.
+    #
+    # @overload initialize(url)
+    #   @param  [String, RDF::URI] url
+    #   @yield  [repository]
+    #   @yieldparam [Repository]
+    #
+    # @overload initialize(options = {})
+    #   @param  [Hash{Symbol => Object}] options
+    #   @option options [Server] :server (nil)
+    #   @option options [String] :id (nil)
+    #   @option options [String] :title (nil)
+    #   @yield  [repository]
+    #   @yieldparam [Repository]
+    #
+    def initialize(url_or_options, &block)
+      case url_or_options
+        when String
+          initialize(RDF::URI.new(url_or_options), &block)
+        when RDF::URI
+          require 'addressable/uri' unless defined?(Addressable)
+          require 'pathname' unless defined?(Pathname)
+          @uri     = url_or_options
+          @server  = Server.new(RDF::URI.new(Addressable::URI.new({
+            :scheme   => @uri.scheme,
+            :userinfo => @uri.userinfo,
+            :host     => @uri.host,
+            :port     => @uri.port,
+            :path     => Pathname.new(@uri.path).parent.parent.to_s, # + '../..'
+          })))
+          @options = {}
+        when Hash
+          raise ArgumentError.new("missing options[:server]") unless url_or_options.has_key?(:server)
+          raise ArgumentError.new("missing options[:id]")     unless url_or_options.has_key?(:id)
+          @options = url_or_options.dup
+          @server  = @options.delete(:server)
+          @id      = @options.delete(:id)
+          @uri     = @options.delete(:uri) || server.url("repositories/#{@id}")
+          @title   = @options.delete(:title)
+        else
+          raise ArgumentError.new("wrong argument type #{url_or_options.class} (expected String, RDF::URI or Hash)")
+      end
 
       if block_given?
         case block.arity
@@ -31,4 +85,4 @@ module RDF module Sesame
       end
     end
   end
-end end
+end
--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -1,5 +1,55 @@
 require 'rdf/sesame'
 
 describe RDF::Sesame::Repository do
-  # TODO
+  before :each do
+    @url    = RDF::URI.new(ENV['SESAME_URL'] || "http://localhost:8080/openrdf-sesame")
+    @server = RDF::Sesame::Server.new(@url)
+  end
+
+  context "when created" do
+    it "should require exactly one argument" do
+      lambda { RDF::Sesame::Repository.new }.should raise_error(ArgumentError)
+      lambda { RDF::Sesame::Repository.new(nil, nil) }.should raise_error(ArgumentError)
+    end
+
+    it "should accept a string argument" do
+      url = "#{@url}/repositories/SYSTEM"
+      lambda { RDF::Sesame::Repository.new(url) }.should_not raise_error(ArgumentError)
+
+      db = RDF::Sesame::Repository.new(url)
+      db.server.to_uri.to_s.should == @url.to_s
+    end
+
+    it "should accept a URI argument" do
+      url = RDF::URI.new("#{@url}/repositories/SYSTEM")
+      lambda { RDF::Sesame::Repository.new(url) }.should_not raise_error(ArgumentError)
+
+      db = RDF::Sesame::Repository.new(url)
+      db.server.to_uri.to_s.should == @url.to_s
+    end
+
+    it "should accept :server and :id" do
+      options = {:server => @server, :id => :SYSTEM}
+      lambda { RDF::Sesame::Repository.new(options) }.should_not raise_error(ArgumentError)
+
+      db = RDF::Sesame::Repository.new(options)
+      db.server.to_uri.to_s.should == @url.to_s
+    end
+
+    it "should reject :server without :id" do
+      options = {:server => @server}
+      lambda { RDF::Sesame::Repository.new(options) }.should raise_error(ArgumentError)
+    end
+
+    it "should reject :id without :server" do
+      options = {:id => :SYSTEM}
+      lambda { RDF::Sesame::Repository.new(options) }.should raise_error(ArgumentError)
+    end
+
+    it "should reject any other argument" do
+      [nil, :SYSTEM, 123, []].each do |value|
+        lambda { RDF::Sesame::Repository.new(value) }.should raise_error(ArgumentError)
+      end
+    end
+  end
 end
