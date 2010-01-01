5d99f2d4feac410433eddf6303697c1dff129184
Arto Bendiken <arto@bendiken.net>
2010-01-01 03:28:55 +0100

    Improved RDF::Sesame documentation.

--- lib/rdf/sesame.rb
+++ lib/rdf/sesame.rb
@@ -2,17 +2,37 @@ require 'rdf'
 
 module RDF
   ##
-  # RDF::Sesame is a Sesame 2.0 adapter for RDF.rb.
+  # **`RDF::Sesame`** is a Sesame 2.0 adapter for RDF.rb.
   #
-  # @example Installing the RDF::Sesame plugin
-  #   $ sudo gem install rdf-sesame
+  # Dependencies
+  # ------------
   #
-  # @example Requiring the RDF::Sesame plugin
+  # * [RDF.rb](http://rdf.rubyforge.org/) (>= 0.0.6)
+  # * [JSON](http://flori.github.com/json/) (>= 1.2.0)
+  #
+  # Installation
+  # ------------
+  #
+  # The recommended installation method is via RubyGems. To install the latest
+  # official release from Gemcutter, do:
+  #
+  #     % [sudo] gem install rdf-sesame
+  #
+  # Documentation
+  # -------------
+  #
+  # * {RDF::Sesame::Connection}
+  # * {RDF::Sesame::Repository}
+  # * {RDF::Sesame::Server}
+  #
+  # @example Requiring the `RDF::Sesame` module
   #   require 'rdf/sesame'
   #
   # @see http://rdf.rubyforge.org/
   # @see http://www.openrdf.org/
   # @see http://www.openrdf.org/doc/sesame2/system/ch08.html
+  #
+  # @author [Arto Bendiken](http://ar.to/)
   module Sesame
     autoload :Connection, 'rdf/sesame/connection'
     autoload :Repository, 'rdf/sesame/repository'
