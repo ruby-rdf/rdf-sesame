bf46baa87dca5903960f88095f4a370c3c80c028
Aymeric Brisse <aymeric.brisse@perfect-memory.com>
2013-07-23 11:14:45 +0200

    Exceptions  are now located in RDF::Sesame
    Signed-off-by: Aymeric Brisse <aymeric.brisse@perfect-memory.com>

--- lib/rdf/sesame/exceptions.rb
+++ lib/rdf/sesame/exceptions.rb
@@ -1,7 +1,5 @@
-module RDF
-  class Repository
-    class ClientError < StandardError; end
-    class MalformedQuery < ClientError; end
-    class ServerError < StandardError; end
-  end
+module RDF::Sesame
+  class ClientError < StandardError; end
+  class MalformedQuery < ClientError; end
+  class ServerError < StandardError; end
 end
