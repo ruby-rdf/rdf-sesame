7c99ca23006439e37958be2faaa38612d0e0159c
Karim Bogtob <karim.bogtob@perfect-memory.com>
2016-05-19 12:30:10 +0200

    Refactor version specs to suit RSpec 3 syntax.

--- spec/lib/rdf/sesame/version_spec.rb
+++ spec/lib/rdf/sesame/version_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 describe 'RDF::Sesame::VERSION' do
+  let(:version_file_path) do
+    File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'VERSION')
+  end
+
+  let(:version) do
+    File.read(version_file_path).chomp
+  end
+
   it "should match the VERSION file" do
-    version_file_path = File.join(File.dirname(__FILE__), '..', '..', '..', '..', 'VERSION')
-    RDF::Sesame::VERSION.to_s.should == File.read(version_file_path).chomp
+    expect(RDF::Sesame::VERSION.to_s).to eq version
   end
 end
